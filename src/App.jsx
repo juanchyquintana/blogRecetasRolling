45bcf0596714a20ca261c941b91800e9dfcb8afb
sheyla astorga <sheylaastorga1998@gmail.com>
2024-02-24 14:39:24 -0300

    agregue el banner del la seccion detalle de producto

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Inicio from "./components/pages/Inicio";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-// import DetalleProducto from "./components/pages/DetalleProducto";
+import DetalleProducto from "./components/pages/DetalleProducto";
 
 function App() {
   return (
     <>
-      <Header />
-      <Inicio></Inicio>
-      {/* <Route exact path='/DetalleProducto' element={<DetalleProducto></DetalleProducto>}></Route> */}
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<DetalleProducto></DetalleProducto>}
+          ></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+      {/* <Inicio></Inicio> */}
     </>
   );
 }
--- src/components/pages/DetalleProducto.jsx
+++ src/components/pages/DetalleProducto.jsx
@@ -1,10 +1,16 @@
-
+import { Container } from "react-bootstrap";
+import lasagna from "../../assets/lasagna.jpg";
 const DetalleProducto = () => {
-    return (
-        <>
-            
-        </>
-    );
+  return (
+    <Container className="mt-5 d-flex justify-content-between "style={{ height: '400px' }}>
+      <div className="bannerProducto w-100 text-center  ">
+        <h2 className="text-light titulosFuente">{"Lasagna Boloñesa"}</h2>
+        <p className="text-light">"Aprende a preparar esta receta de Lasagna a la boloñesa"</p>
+        <p className="text-light">Un plato italiano clásico conocido por sus capas de pasta intercaladas con una rica mezcla de carne y salsas. En este caso, la lasagna a la boloñesa se destaca por su relleno de ragú boloñés, una salsa de carne que proviene de la región de Bolonia, Italia.</p>
+      </div>
+      <img src={lasagna} alt="lasagna" className="img-fluid w-50 "/>
+    </Container>
+  );
 };
 
 export default DetalleProducto;
