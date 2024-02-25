ad6b076394207fc0139060bfa13b53ddeca946f0
Natalia Morales <natii.vmorales98@gmail.com>
2024-02-25 17:47:10 -0300

    se agrego el estilo de la página Administrador

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,31 +1,38 @@
-import { Table } from "react-bootstrap";
+import { Button, Table } from "react-bootstrap";
 
 const Administrador = () => {
-    return (
-        <section>
-        <h1 className='text-center'>Recetas disponibles</h1>
-        <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Código</th>
-          <th>Nombre</th>
-          <th>Descripción</th>
-          <th>Ingredientes</th>
-          <th>Preparación</th>
-          <th>Categoria</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-      </tbody>
-    </Table>
-        </section>
-    );
+  return (
+    <section className="container my-4">
+      <div className="d-flex justify-content-between align-items-start">
+        <h1 className="mb-3">Recetas disponibles</h1>
+        <Button variant="primary p-2">+</Button>
+      </div>
+      <Table striped bordered hover className="mt-4">
+        <thead>
+          <tr>
+            <th>Código</th>
+            <th>Nombre</th>
+            <th>URL Imagen</th>
+            <th>Descripción</th>
+            <th>Ingredientes</th>
+            <th>Preparación</th>
+            <th>Categoria</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>nombre</td>
+            <td>imagen</td>
+            <td>descripcion</td>
+            <td>ingredientes</td>
+            <td>preparacion</td>
+            <td>categoria</td>
+          </tr>
+        </tbody>
+      </Table>
+    </section>
+  );
 };
 
 export default Administrador;
