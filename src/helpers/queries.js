d89151c84a31be9ad9ff0f2fe2d009cf09aee3e4
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-02-27 11:11:45 -0300

    se comenzó a crear la funcionaldiad de editarReceta

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -1,7 +1,20 @@
-const crearProducto = async () => {
+const crearProducto = async () => {};
 
-}
+const editarReceta = async (recetaModificada, id) => {
+  try {
+    const respuesta = await fetch(`${NOMBRE_VARIABLE}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(recetaModificada),
+    });
 
-export {
-    crearProducto
-}
+    console.log(respuesta);
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { crearProducto, editarReceta };
