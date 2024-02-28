720102f5d839f79248fb88224027ef736bb892c4
Natalia Morales <natii.vmorales98@gmail.com>
2024-02-28 00:22:34 -0300

    se agrego el estilo de la pagina error404

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+import error from "../../assets/error.jpg"
 
 const Error404 = () => {
     return (
-        <section className="container text-center my-5">
-            <h1 className="mb-5 fw-bolder tamanioLetraError">Error 404</h1>
-            <Link to={"/"} className="btn btn-success">Página principal</Link>
+        <section className="container text-center my-4 imagenFondoError">
+            <img src={error} alt="imgError" className="img-fluid" style={{ width: "25%", objectFit: "cover" }}/>
+            <h1 className="mb-1 fw-bolder tamanioLetraError">Error 404</h1>
+            <Link to={"/"} className="btn btn-success" >Página principal</Link>
+            
         </section>
     );
 };
